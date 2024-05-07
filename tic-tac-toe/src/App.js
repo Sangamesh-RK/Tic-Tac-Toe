0060ddae8b836674fc0020eea7429f3de3e160de
Sangamesh-RK <sangamesh99998@gmail.com>
2024-05-08 00:56:45 +0530

    Implement Basic Gameplay

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -11,6 +11,13 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner : ' + winner;
+  } else {
+    status = (xIsNext ? 'X' : 'O') + "'s Turn";
+  }
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -49,6 +56,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
